92d9447f730efe4972cde8afc3e5d0e833ad23cc
julzedz <edoziechukwuma@gmail.com>
2024-01-24 20:41:38 +0100

    Add after_create callback to set default values for Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,10 @@
 class Account < ApplicationRecord
   belongs_to :user
+  after_create :set_default_values
+
+private
+
+  def set_default_values
+    self.update_columns(savings_account: 0.00, investment: 0.00, earnings: 0.00, stakes: 0.00)
+  end
 end
