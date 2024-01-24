24c7e790fdb1a0f836294186395cded4d2438a08
julzedz <edoziechukwuma@gmail.com>
2024-01-24 20:35:13 +0100

    Add account model and association with user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_24_180743) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_24_193050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "savings_account"
+    t.decimal "investment"
+    t.decimal "earnings"
+    t.decimal "stakes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_24_180743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
 end
