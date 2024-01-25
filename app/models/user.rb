3a168097b260e6f71ec385e923b2cf37393478df
julzedz <edoziechukwuma@gmail.com>
2024-01-25 12:03:03 +0100

    Add password getter and setter methods, set address and generate account number on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 
+# Override password setter method to store plain text password in the database
+  def password=(new_password)
+    @password = new_password
+    self.encrypted_password = Devise.bcrypt(self.class, new_password) unless Rails.env.development?
+  end
+
+# Override password getter method to return plain text password
+  def password
+    @password
+  end
+
+  before_save :set_address
+  before_create :generate_account_number
+
   has_one :account
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
@@ -15,6 +29,14 @@ class User < ApplicationRecord
   validates :state, presence: true
   validates :country, presence: true
   validates :profile_img_path, presence: true
-  validates :fullname, presence: true
-  validates :address, presence: true
+
+  private
+
+  def set_address
+    self.address = "#{city}, #{state}, #{country}"
+  end
+
+  def generate_account_number
+    self.account_number = rand(1_000_000_000..9_999_999_999)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create two users
+user1 = User.create!(
+  email: 'user1@example.com',
+  password: 'password_digest',
+  first_name: 'John',
+  last_name: 'Doe',
+  phone_number: '1234567890',
+  city: 'New York',
+  state: 'NY',
+  date_of_birth: '1980-01-01',
+  country: 'USA',
+  profile_img_path: '/path/to/image1.jpg',
+)
+
+user2 = User.create!(
+  email: 'user2@example.com',
+  password: 'password_digest',
+  first_name: 'Jane',
+  last_name: 'Doe',
+  phone_number: '0987654321',
+  city: 'Los Angeles',
+  state: 'CA',
+  date_of_birth: '1990-01-01',
+  country: 'USA',
+  profile_img_path: '/path/to/image2.jpg',
+)
+
+# Create accounts for the users
+Account.create!(
+  user_id: user1.id,
+  savings_account: 1000.00,
+  investment: 2000.00,
+  earnings: 300.00,
+  stakes: 400.00
+)
+
+Account.create!(
+  user_id: user2.id,
+  savings_account: 2000.00,
+  investment: 3000.00,
+  earnings: 400.00,
+  stakes: 500.00
+)
