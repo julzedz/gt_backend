474df68519e5adc143b4eb0c7ebae13efa7a5519
julzedz <edoziechukwuma@gmail.com>
2024-01-25 14:49:05 +0100

    Add authentication to accounts and users controllers

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::AccountsController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
+# GET /api/v1/accounts
+  def index
+    @accounts = current_user.account
+
+    render json: @accounts, include: [:user]
+  end
+
+# GET /api/v1/accounts/:id
+  def show
+    @account = Account.find(params[:id])
+
+    render json: @account, include: [:user]
+  end
+
+# POST /api/v1/accounts
+  def create
+    @account = Account.new(account_params)
+    @account.user = current_user
+
+    if @account.save
+      render json: @account, include: [:user], status: :created
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+# PATCH /api/v1/accounts/:id
+  def update
+    @account = current_user.account
+
+    if @account.update(account_params)
+      render json: @account, include: [:user]
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+# Whitelist permissible params
+  def account_params
+    params.permit(:savings_account, :investment, :earnings, :stakes)
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
+
+  # GET /api/v1/users/me
+  def show_current
+    @user = current_user
+
+    render json: @user, only: [:email, :phone_number, :first_name, :date_of_birth, :city, :state, :country, :profile_img_path, :address, :fullname, :account_number]
+  end
+
+# GET /api/v1/users/:id
+  def show
+    @user = User.find(params[:id])
+
+    # Adjust fields based on access control/visibility preferences
+    render json: @user, only: [:email, :phone_number, :first_name, :date_of_birth, :city, :state, :country, :profile_img_path, :address, :fullname, :account_number]
+  end
+
+private
+
+  def user_params
+    params.permit(:email, :phone_number, :first_name, :date_of_birth, :city, :state, :country, :profile_img_path, :address, :fullname, :account_number)
+  end
 end
