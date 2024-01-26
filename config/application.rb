9a0ca15d1a4bb675224b666570241de447ba1d3d
julzedz <edoziechukwuma@gmail.com>
2024-01-26 06:42:10 +0100

    Update CORS origins to http://localhost:3001

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module GtBackend
     # Add CORS middleware
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3001'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
