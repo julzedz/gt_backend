74441a42bb63dc88c96c3835dc06dc9a79c32d31
julzedz <edoziechukwuma@gmail.com>
2024-02-05 23:32:46 +0100

    Add authentication controller for API v1

---
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class AuthenticateController < ApplicationController
+      # your code here
+      before_action :authenticate_request, except: :login
+
+      # POST /login
+      def login
+        @user = User.find_by(email: login_params[:email])
+        if @user&.authenticate(login_params[:password])
+          token = JsonWebToken.encode(user_id: @user.id)
+          time = Time.now + 7.days.to_i
+          render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+            user: @user }, status: :ok
+        else
+          render json: { error: 'unauthorized' }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def login_params
+        params.permit( :email, :password )
+      end
+    end
+  end
+end
