9793c413e04dea6bb83c35db468672ecf6d3020f
julzedz <edoziechukwuma@gmail.com>
2025-06-04 15:45:40 +0100

    Add otp routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
       post 'login', to: 'authenticate#login'
+      post 'verify-otp', to: 'authenticate#verify_otp'
+      post 'resend-otp', to: 'authenticate#resend_otp'
+      resources :users
       resources :accounts
     end
   end
